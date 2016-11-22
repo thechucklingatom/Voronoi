6858a2e10cc68a265df5653460c226c2e6d310c1
Robert Putnam <thechucklingatom@gmail.com>
2016-11-21 22:14:19 -0700

    String stuff

--- src/main/java/CSCI432/Voronoi/MapsAPIAccesor.java
+++ src/main/java/CSCI432/Voronoi/MapsAPIAccesor.java
@@ -8,5 +8,8 @@ import com.google.gson.Gson;
  * Created by thechucklingatom on 11/20/2016.
  */
 public class MapsAPIAccesor {
+	private final String API_KEY = "lol";
+	private final String MAP_ENDPOINT = "";
+	private final String PLACES_ENDPOINT = "";
 	//TODO Add logic for accessing the google maps api for location and pictures.
 }
